3a30737f42be57194a27ea1b232ae1a84241f528
aswanth9495 <aswanth@scaler.com>
2025-05-11 19:36:58 +0530

    feat: change vite config

--- packages/resume-builder/vite.config.js
+++ packages/resume-builder/vite.config.js
@@ -9,7 +9,7 @@ const __dirname = dirname(__filename);
 export default defineConfig({
   plugins: [
     react({
-      jsxRuntime: 'classic',
+      jsxRuntime: 'automatic',
     }),
   ],
   css: {
